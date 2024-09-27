d638ad94be43bf88b3c63138099f67fa0350ecf3
Myre36 <67600948+Myre36@users.noreply.github.com>
2024-09-27 12:44:37 +0200

    Don't remeber what I did

--- Assets/Scripts/spawnObstacles.cs
+++ Assets/Scripts/spawnObstacles.cs
@@ -22,10 +22,6 @@ public class spawnObstacles : MonoBehaviour
     private bool spawnThreeSafe;
     private bool spawnFourSafe;
 
-    //The minimum time needed for spawning the scrap
-    public int scrapTime;
-    private int randomScrapTime;
-
     void Start()
     {
         //Calls in a function to restart all the bools
@@ -196,10 +192,5 @@ public class spawnObstacles : MonoBehaviour
         RestartBools();
         //Restarts the coroutine
         StartCoroutine(SpawnRandomObstacle());
-    }
-    IEnumerator SpawnScrap()
-    {
-        
-        yield return new WaitForSeconds(0.1f);
     }
 }
